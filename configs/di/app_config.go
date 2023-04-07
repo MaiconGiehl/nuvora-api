dd8ea97d892c18ecbb77d18f5740ea02fde6fd39
Maicon <maicongiehl@gmail.com>
2023-04-06 22:00:33 -0300

    chore: implement usecases in di

--- configs/di/app_config.go
+++ configs/di/app_config.go
@@ -4,18 +4,21 @@ import (
 	"context"
 	"database/sql"
 
-	login_as_company_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/company/login"
-	get_last_purchases_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/customer/get-last-purchases"
-	get_possible_travels_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/customer/get-possible-travels"
-	login_as_customer_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/customer/login"
-
 	account_entity "github.com/maicongiehl/nuvora-api/internal/infra/dataprovider/sql/pg/account"
 	company_entity "github.com/maicongiehl/nuvora-api/internal/infra/dataprovider/sql/pg/company"
 	customer_entity "github.com/maicongiehl/nuvora-api/internal/infra/dataprovider/sql/pg/customer"
-
 	person_entity "github.com/maicongiehl/nuvora-api/internal/infra/dataprovider/sql/pg/person"
 	ticket_entity "github.com/maicongiehl/nuvora-api/internal/infra/dataprovider/sql/pg/ticket"
 	travel_entity "github.com/maicongiehl/nuvora-api/internal/infra/dataprovider/sql/pg/travel"
+
+	create_employee_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/company/create-employee"
+	get_employees_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/company/get-employees"
+	get_employees_tickets_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/company/get-employees-tickets"
+	login_as_company_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/company/login"
+
+	get_last_purchases_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/customer/get-last-purchases"
+	get_possible_travels_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/customer/get-possible-travels"
+	login_as_customer_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/customer/login"
 )
 
 func SetupDIConfig(
@@ -27,14 +30,25 @@ func SetupDIConfig(
 	newCompanyPGSQLRepository := company_entity.NewCompanyPGSQLRepository(ctx, db)
 	newPersonPGSQLRepository := person_entity.NewPersonPGSQLRepository(ctx, db)
 	newAccountPGSQLRepository := account_entity.NewAccountPGSQLRepository(ctx, db)
+	newTicketPGSQLRepository := ticket_entity.NewTicketPGSQLRepository(ctx, db)
 	
 	newTravelPGSQLRepository := travel_entity.NewTravelPGSQLRepository(ctx, db)
 
-	newLoginAsCustomerUseCase := login_as_customer_usecase.NewLoginAsCustomerUseCase(
-		ctx, 
+	// Company UseCases
+	newCreateEmployeeUseCase := create_employee_usecase.NewCreateEmployeeUseCase(ctx, 
 		newCustomerPGSQLRepository, 
 		newPersonPGSQLRepository,
 		newAccountPGSQLRepository,
+	)
+
+	newGetEmployeesTicketsUseCase := get_employees_tickets_usecase.NewGetEmployeesTicketsUseCase(
+		ctx,
+		newTicketPGSQLRepository,
+	)
+
+	newGetEmployeesUseCase := get_employees_usecase.NewGetEmployeesUseCase(
+		ctx,
+		newCustomerPGSQLRepository,
 	)
 
 	newLoginAsCompanyUseCase := login_as_company_usecase.NewLoginAsCompanyUseCase(
@@ -43,6 +57,16 @@ func SetupDIConfig(
 		newPersonPGSQLRepository,
 		newAccountPGSQLRepository,
 	)
+
+	// Customer UseCases
+	newLoginAsCustomerUseCase := login_as_customer_usecase.NewLoginAsCustomerUseCase(
+		ctx, 
+		newCustomerPGSQLRepository, 
+		newPersonPGSQLRepository,
+		newAccountPGSQLRepository,
+	)
+
+	newGetLastPurchasesUseCase := get_last_purchases_usecase.NewGetLastPurchasesUsecase(ctx, newTicketPGSQLRepository)
 
 	newGetPossibleTravelsUseCase := get_possible_travels_usecase.NewGetPossibleTravelsUseCase(
 		ctx, 
@@ -53,13 +77,14 @@ func SetupDIConfig(
 		newTravelPGSQLRepository,
 	)
 
-	newTicketPGSQLRepository := ticket_entity.NewTicketPGSQLRepository(ctx, db)
-	newGetLastPurchasesUseCase := get_last_purchases_usecase.NewGetLastPurchasesUsecase(ctx, newTicketPGSQLRepository)
 
 	return &App{
-		LoginAsCustomerUseCase: newLoginAsCustomerUseCase,
-		LoginAsCompanyUseCase: newLoginAsCompanyUseCase,
-		GetPossibleTravelsUseCase: newGetPossibleTravelsUseCase,
+		CreateEmployeeUseCase: newCreateEmployeeUseCase,
+		GetEmployeesTicketsUsecase: *newGetEmployeesTicketsUseCase,
+		GetEmployeesUseCase: newGetEmployeesUseCase,
 		GetLastPurchasesUseCase: newGetLastPurchasesUseCase,
+		GetPossibleTravelsUseCase: newGetPossibleTravelsUseCase,
+		LoginAsCompanyUseCase: newLoginAsCompanyUseCase,
+		LoginAsCustomerUseCase: newLoginAsCustomerUseCase,
 	}
 }
--- configs/di/setup.go
+++ configs/di/setup.go
@@ -1,17 +1,22 @@
 package setup_di_config
 
 import (
+	create_employee_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/company/create-employee"
+	get_employees_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/company/get-employees"
+	get_employees_tickets_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/company/get-employees-tickets"
+	login_as_company_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/company/login"
 	get_last_purchases "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/customer/get-last-purchases"
 	get_possible_travels_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/customer/get-possible-travels"
-
-	login_as_company_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/company/login"
-	login__as_customer_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/customer/login"
+	login_as_customer_usecase "github.com/maicongiehl/nuvora-api/internal/core/application/usecase/customer/login"
 )
 
 type App struct {
-	LoginAsCustomerUseCase login__as_customer_usecase.LoginAsCustomerUseCaseInterface
-	LoginAsCompanyUseCase login_as_company_usecase.LoginAsCompanyUseCaseInterface
-	GetPossibleTravelsUseCase  get_possible_travels_usecase.GetPossibleTravelsUseCaseInterface
+	CreateEmployeeUseCase create_employee_usecase.CreateEmployeeUseCaseInterface
+	GetEmployeesTicketsUsecase get_employees_tickets_usecase.GetEmployeesTicketsUseCase  
+	GetEmployeesUseCase get_employees_usecase.GetEmployeesUseCaseInterface
 	GetLastPurchasesUseCase get_last_purchases.GetLastPurchasesUseCaseInterface
+	GetPossibleTravelsUseCase  get_possible_travels_usecase.GetPossibleTravelsUseCaseInterface
+	LoginAsCompanyUseCase login_as_company_usecase.LoginAsCompanyUseCaseInterface
+	LoginAsCustomerUseCase login_as_customer_usecase.LoginAsCustomerUseCaseInterface
 }
 
